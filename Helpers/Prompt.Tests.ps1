cc1a121326e2921c2f4c480a48701a47755033e1
Jan De Dobbeleer <jan.de.dobbeleer@gmail.com>
2016-10-24 08:32:10 +0200

    Added tests for Get-Home and Get-Provider

--- Helpers/Prompt.Tests.ps1
+++ Helpers/Prompt.Tests.ps1
@@ -5,31 +5,47 @@ $sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path).Replace(".Tests.", ".")
 Describe "Test-IsVanillaWindow" {
     BeforeEach { Remove-Item Env:\PROMPT -ErrorAction SilentlyContinue
                  Remove-Item Env:\ConEmuANSI -ErrorAction SilentlyContinue }
-    # $env:PROMPT -or $env:ConEmuANSI
-    It "runs in ConEmu and outputs 'false'" {
+    Context "Running in a non-vanilla window" {
+        It "runs in ConEmu and outputs 'false'" {
         $env:PROMPT = $false
         $env:ConEmuANSI = $true
         Test-IsVanillaWindow | Should Be $false
+        }
+        It "runs in cmder and outputs 'false'" {
+            $env:ConEmuANSI = $false
+            $env:PROMPT = $true
+            Test-IsVanillaWindow | Should Be $false
+        }
+        It "runs in ConEmu and outputs 'false'" {
+            $env:ConEmuANSI = $true
+            Test-IsVanillaWindow | Should Be $false
+        }
+        It "runs in cmder and outputs 'false'" {
+            $env:PROMPT = $true
+            Test-IsVanillaWindow | Should Be $false
+        }
+        It "runs in cmder and conemu and outputs 'false'" {
+            $env:PROMPT = $true
+            $env:ConEmuANSI = $true
+            Test-IsVanillaWindow | Should Be $false
+        }
     }
-    It "runs in cmder and outputs 'false'" {
-        $env:ConEmuANSI = $false
-        $env:PROMPT = $true
-        Test-IsVanillaWindow | Should Be $false
+    Context "Running in a vanilla window" {
+        It "runs in a vanilla window and outputs 'true'" {
+           Test-IsVanillaWindow | Should Be $true
+        }
     }
-    It "runs in ConEmu and outputs 'false'" {
-        $env:ConEmuANSI = $true
-        Test-IsVanillaWindow | Should Be $false
-    }
-    It "runs in cmder and outputs 'false'" {
-        $env:PROMPT = $true
-        Test-IsVanillaWindow | Should Be $false
-    }
-    It "runs in cmder and conemu and outputs 'false'" {
-        $env:PROMPT = $true
-        $env:ConEmuANSI = $true
-        Test-IsVanillaWindow | Should Be $false
-    }
-    It "runs in a vanilla window and outputs 'true'" {
-        Test-IsVanillaWindow | Should Be $true
-    }
+}
+
+Describe "Get-Home" {
+    It "returns $HOME" {
+           Get-Home | Should Be $HOME
+        }
+}
+
+Describe "Get-Provider" {
+    It "uses the provider 'AwesomeSauce'" {
+           Mock Get-Item { return @{PSProvider = @{Name = 'AwesomeSauce'}} }
+           Get-Provider $pwd | Should Be 'AwesomeSauce'
+        }
 }
